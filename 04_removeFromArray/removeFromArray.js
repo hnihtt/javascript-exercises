4e89679daab9f8f930148b13389a2a6069fa428c
Thinh <vinhthinhvo@gmail.com>
2024-06-20 18:22:04 +0700

    Done 4 5 6 7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    // const fucntionArg = Array.from(arguments);
+    // const arrayTaken = fucntionArg[0];
+    // const listToRemove = [];
+    // for (let i = 1; i < arguments.length; i++) {
+    //     listToRemove.push(arguments[i]);
+    // }
+    // const result = arrayTaken.filter(item => !listToRemove.includes(item))
+    // return result
+    return array.filter(check => !args.includes(check))
 };
 
 // Do not edit below this line
